568681c8632660998e035566157def1e03fe4805
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-10-10 19:18:12 +0300

    Initial Commit

---
+++ YOLO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YOLO
+//
+//  Created by MacBook on 10.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
