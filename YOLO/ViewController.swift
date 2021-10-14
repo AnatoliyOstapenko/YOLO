4791f1bc7a623dc1bb83af9f0a5604eeb7fc156f
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-10-14 21:13:48 +0300

    update ML model + Vision block

--- YOLO/ViewController.swift
+++ YOLO/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var yoloImage: UIImageView!
+    @IBOutlet weak var yoloLabel: UILabel!
+    
+   
+    let imagePicker = UIImagePickerController()
+    var frenchDogCoreMLModel = FrenchDogCoreMLModel()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        frenchDogCoreMLModel.frenchDogDelegate = self
+        
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+  
+    
+    
 }
+
+//MARK: - UIImage Picker Controller Delegate, UINavigation Controller Delegate
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        
+        yoloImage.image = image
+        
+        // dispatch image to FrenchDogCoreMLModel
+        frenchDogCoreMLModel.imageClassifier(image)
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+}
+//MARK: French Dog Delegate
+extension ViewController: FrenchDogDelegate {
+    func getFirstResult(_ item: String) {
+        DispatchQueue.main.async {
+            self.navigationItem.title = item
+        }
+    }
+    
+    
+}
+
 
